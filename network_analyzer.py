d3a79ad573b92c31428b90a77164cf574c7f9633
andy <andyestevez27@gmail.com>
2026-02-16 20:24:42 -0500

    Capture packets from network interface

--- network_analyzer.py
+++ network_analyzer.py
@@ -7,3 +7,7 @@ print(conf.ifaces)
 # Software Loopback Interface 1
 # Realtek Gaming 2.5GbE Family Controller
 # VirtualBox Host-Only Ethernet Adapter
+
+# 1. Learning how to capture packets from network interfaces using sniff()
+sniff(count=5, iface=conf.iface) # or grabbing from results of conf.ifaces
+print("")
