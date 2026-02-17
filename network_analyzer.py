b30c66c25c6957eab54f4b754927c5ec5324eb6e
andy <andyestevez27@gmail.com>
2026-02-17 18:36:22 -0500

    capture packets based on total or duration

--- network_analyzer.py
+++ network_analyzer.py
@@ -11,3 +11,10 @@ print(conf.ifaces)
 # 1. Learning how to capture packets from network interfaces using sniff()
 sniff(count=5, iface=conf.iface) # or grabbing from results of conf.ifaces
 print("")
+
+# 2. Captures live packets for a specified duration or packet count
+count_packets = sniff(filter="tcp", count=5, iface="Realtek Gaming 2.5GbE Family Controller")
+print("Capturing TCP packets till 5 are received: ", count_packets)
+
+timer_packets = sniff(filter="tcp", timeout=10, iface="Software Loopback Interface 1")
+print("Capturing TCP packets for 10 seconds: ", timer_packets, "\n")
